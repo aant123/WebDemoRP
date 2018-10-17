912856a6b21f8469d598700dcff70a1c020a3b9d
aant123 <42434307+aant123@users.noreply.github.com>
2018-10-17 14:37:57 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
-import {PageHeader} from 'react-bootstrap'
-// import Login from '../src/rp/component/Login'
-import ListIdpContrainer from '../src/rp/contrainer/ListIdpContrainer'
+// import {PageHeader} from 'react-bootstrap'
+import Login from '../src/rp/component/Login'
+// import ListIdpContrainer from '../src/rp/contrainer/ListIdpContrainer'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-      <PageHeader style={{marginLeft:'50px',marginRight:'50px'}}> RP </PageHeader>
-      <ListIdpContrainer/>
+        <Login/>
       </div>
     );
   }
--- src/rp/sagas.js
+++ src/rp/sagas.js
@@ -1,9 +1,8 @@
 import { call, takeEvery, put} from 'redux-saga/effects'
-import { getData } from '../rp/service'
 
+const idpList = [];
 function* fetchAllIdp(){
     try {
-        const idpList = yield call(getData)
         yield put({type: "FETCH_IDP_SUCCEEDED", idpList});
      } catch (e) {
         yield put({type: "FETCH_IDP_FAILED", message: e.message});
