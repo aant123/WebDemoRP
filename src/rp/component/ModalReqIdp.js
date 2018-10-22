679eaa65781537d7125368189757c9b585c1b37a
Nitravee Wongvitavas <nitravee@Nitravees-MacBook-Pro.local>
2018-10-22 17:38:04 +0700

    Edit idp Modal css

--- src/rp/component/ModalReqIdp.js
+++ src/rp/component/ModalReqIdp.js
@@ -1,61 +1,70 @@
-import React, { Component } from 'react';
-import { Container, Modal, Fa, ModalBody, Row } from 'mdbreact'
-
+import React, { Component } from "react";
+import { Container, Modal, Fa, ModalBody, Row, Col } from "mdbreact";
 
 class ModalReqIdp extends Component {
-    componentWillMount(){
-        this.props.reqToIdp()
-    }
-    clickSucess = () => {
-        this.props.sucessModal(true)
-    }
-    render(){
-        const {showLoading} = this.props
-        const textDetailSucess = 'ระบบได้รับการยืนยันและข้อมูลจาก “ตัวตน Idp แล้ว” '
-        const textDetailWaiting = 'ระบบได้ทำการส่งคำร้องขอยืนยันตัวตนและการเข้าถึงข้อมูลไปที่ “ตัวตน Idp” แล้วกรุณาเปิด Appliction ของ “ตัวตน Idp” เพื่อทำการยืนยันคำร้อง'
-        const modalDetail = showLoading? (
-            <div className='text-center'>
-                <Row>
-                    <label className="h6 mb-4">{textDetailWaiting}</label>
-                </Row>
-                <Row className='blue-text' style={{justifyContent: 'center'}}>
-                    <Fa icon="spinner" pulse size="4x" fixed/>
-                    <span className="sr-only" >Loading...</span>
-                </Row>
-            </div>
-        ) : 
-        (
-            <div className='text-center'>
-                <Row>
-                    <label className="h6 mb-4 green-text">{textDetailSucess}</label>
-                </Row>
-                <Row className='green-text' style={{justifyContent: 'center'}}>
-                    <Fa icon='check-circle-o' size='4x' onClick={this.clickSucess}></Fa>
-                </Row>
-            </div>
-        )
-        return(
-            <Container>
-                <Modal isOpen={true} toggle={true}>
-                    <ModalBody style={{marginLeft: 10}}>
-                        <Row>
-                            <label className="h5 mb-4">Reference ID </label>
-                        </Row>
-                        <Row>
-                            <label className="h7 mb-4 blue-text">1425514145551 </label>
-                        </Row>
-                        <Row>
-                            <label className="h5 mb-4">Request Time </label>
-                        </Row>
-                        <Row>
-                            <label className="h7 mb-4 blue-text">15 ตุลาคม 2561 14:00</label>
-                        </Row>
-                            { modalDetail }
-                    </ModalBody>
-                </Modal>
-          </Container>
-        )
-    }
+  componentWillMount() {
+    this.props.reqToIdp();
+  }
+  clickSucess = () => {
+    this.props.sucessModal(true);
+  };
+  render() {
+    const { showLoading } = this.props;
+    const textDetailSucess =
+      "ระบบได้รับการยืนยันและข้อมูลจาก \n “ตัวตน Idp แล้ว” ";
+    const textDetailWaiting =
+      "ระบบได้ทำการส่งคำร้องขอยืนยันตัวตนและการเข้าถึงข้อมูลไปที่ “ตัวตน Idp” แล้วกรุณาเปิด Appliction ของ “ตัวตน Idp” เพื่อทำการยืนยันคำร้อง";
+    const modalDetail = showLoading ? (
+      <div>
+        <Row style={{ marginBottom: '30px', marginTop: '10px' }}>
+          <label className="card-label" style={{ fontSize: '1rem' }}>
+            {textDetailWaiting}
+          </label>
+        </Row>
+        <Row className="blue-text" style={{ justifyContent: "center" }}>
+          <Fa icon="spinner" pulse size="4x" fixed />
+          <span className="sr-only">Loading...</span>
+        </Row>
+      </div>
+    ) : (
+        <div>
+          <Row style={{ marginTop: '10px', marginBottom: '30px'}}>
+            <label className="card-label" style={{ fontSize: '1.1rem', color: '#289123', fontWeight: 'bold' }}>
+              {textDetailSucess}
+            </label>
+          </Row>
+          <Row className="green-text" style={{ justifyContent: "center" }}>
+            <Fa icon="check-circle" size="4x" onClick={this.clickSucess} />
+          </Row>
+        </div>
+      );
+    return (
+      <Container>
+        <Modal isOpen={true} toggle={true} centered>
+          <ModalBody style={{ marginTop: '10px', marginBottom: '10px', marginLeft: '10px', marginRight: '10px' }}>
+            <Col>
+            <Row>
+                <label className="card-label" style={{ fontSize: '1.5rem' }}>ยืนยันตัวตน</label>
+              </Row>
+              <Row>
+                <label className="card-label">Reference ID</label>
+              </Row>
+              <Row>
+                <label className="card-detail">ceeffbb6-60ee-4186-a3ec-102e2576c082</label>
+              </Row>
+              <Row>
+                <label className="card-label">Request Time</label>
+              </Row>
+              <Row>
+                <label className="card-detail">15 ตุลาคม 2561 14:00</label>
+              </Row>
+              {modalDetail}
+            </Col>
+          </ModalBody>
+        </Modal>
+      </Container>
+    );
+  }
 }
 
 export default ModalReqIdp;
