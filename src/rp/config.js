b02c259eefe9daf04a476f904ac28946c5c74b3b
Nitravee Wongvitavas <nitravee@Nitravees-MacBook-Pro.local>
2018-11-05 18:34:35 +0700

    Change Config

--- src/rp/config.js
+++ src/rp/config.js
@@ -1,11 +1,11 @@
-exports.ndidApiCallBackIp = process.env.NDID_API_CALLBACK_IP || 'docker.for.mac.localhost';
-exports.ndidApiCallBackIpClient = process.env.NDID_API_CALLBACK_IP || 'localhost';
+exports.ndidApiCallBackIp = process.env.NDID_API_CALLBACK_IP || 'tuaton.newkru-cloud.com';
+exports.ndidApiCallBackIpClient = process.env.NDID_API_CALLBACK_IP || 'tuaton.newkru-cloud.com';
 exports.ndidApiCallBackPort = process.env.NDID_API_CALLBACK_PORT || 5000;
 
-exports.ndidServerIp = process.env.NDID_SERVER_IP || 'localhost';
+exports.ndidServerIp = process.env.NDID_SERVER_IP || 'tuaton.newkru-cloud.com';
 exports.ndidServerPort = process.env.NDID_SERVER_PORT || 5000;
 
-exports.ndidApiMasterIp = process.env.NDID_API_MASTER_IP || 'localhost';
+exports.ndidApiMasterIp = process.env.NDID_API_MASTER_IP || 'tuaton.newkru-cloud.com';
 exports.ndidApiMasterPort = process.env.NDID_API_MASTER_PORT || 8200
 
-exports.timeout = 120
+exports.timeout = 10
